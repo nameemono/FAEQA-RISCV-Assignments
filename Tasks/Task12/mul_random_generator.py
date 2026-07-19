4f1eef409b42abd708faab42556db5c591a24f51
Nameemono <nameemono@gmail.com>
2026-07-19 15:28:40 +0000

    fix(Task12): improve random MUL instruction generation

--- Tasks/Task12/mul_random_generator.py
+++ Tasks/Task12/mul_random_generator.py
@@ -17,12 +17,16 @@ with open("mul_random.S", "w") as f:
 
     for i in range(NUM_TESTS):
 
-        rs1 = random.randint(1, 31)
-        rs2 = random.randint(1, 31)
-        rd  = random.randint(1, 31)
+        while True:
+            rs1 = random.randint(1, 31)
+            rs2 = random.randint(1, 31)
+            rd = random.randint(1, 31)
 
-        value1 = random.randint(0, 100)
-        value2 = random.randint(0, 100)
+            if len({rs1, rs2, rd}) == 3:
+                break
+
+        value1 = random.randint(-1000, 1000)
+        value2 = random.randint(-1000, 1000)
 
         f.write(f"# Test {i+1}\n")
         f.write(f"li x{rs1}, {value1}\n")
